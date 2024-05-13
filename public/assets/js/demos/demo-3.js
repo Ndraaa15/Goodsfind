d6c64ccb4ed96f157b06110505e5c0f347bd5eb3
Gede Indra Adi Brata <indrabrata599@gmail.com>
2024-05-13 20:27:26 +0700

    Delete depecrated file

--- public/assets/js/demos/demo-3.js
+++ public/assets/js/demos/demo-3.js
@@ -1,24 +1,40 @@
 // Demo 3 Js file
 $(document).ready(function () {
-    'use strict';
+    "use strict";
 
     // Deal of the day countdown
-	if ( $.fn.countdown ) {
-		$('.deal-countdown').each(function () {
-			var $this = $(this), 
-				untilDate = $this.data('until'),
-				compact = $this.data('compact');
+    if ($.fn.countdown) {
+        $(".deal-countdown").each(function () {
+            var $this = $(this),
+                untilDate = $this.data("until"),
+                compact = $this.data("compact");
 
-			$this.countdown({
-			    until: untilDate, // this is relative date +10h +5m vs..
-			    format: 'HMS',
-			    padZeroes: true,
-			    labels: ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds'],
-			    labels1: ['year', 'month', 'week', 'day', 'hour', 'minutes', 'second']
-			});
-		});
+            $this.countdown({
+                until: untilDate, // this is relative date +10h +5m vs..
+                format: "HMS",
+                padZeroes: true,
+                labels: [
+                    "years",
+                    "months",
+                    "weeks",
+                    "days",
+                    "hours",
+                    "minutes",
+                    "seconds",
+                ],
+                labels1: [
+                    "year",
+                    "month",
+                    "week",
+                    "day",
+                    "hour",
+                    "minutes",
+                    "second",
+                ],
+            });
+        });
 
-		// Pause
-		// $('.deal-countdown').countdown('pause');
-	}
+        // Pause
+        // $('.deal-countdown').countdown('pause');
+    }
 });
